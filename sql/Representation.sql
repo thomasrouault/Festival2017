a0ad8d2c506894d6e3db2dece3deb48600174ee9
tberthome <tberthome@btssio-VirtualBox>
2017-11-16 15:14:18 +0100

    Modification Bdd (representation)

---
+++ sql/Representation.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Jeu 16 Novembre 2017 à 15:12
+-- Version du serveur :  5.7.19-0ubuntu0.16.04.1
+-- Version de PHP :  7.0.22-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `festival`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Representation`
+--
+
+CREATE TABLE `Representation` (
+  `Lieu` varchar(100) DEFAULT NULL,
+  `Groupe` varchar(100) DEFAULT NULL,
+  `HeureDebut` time DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  `HeureFin` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
