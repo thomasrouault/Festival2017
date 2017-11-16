0f3461a4e40befc5963a4e61d017c9c2eeb5c232
trouault <trouault@btssio-VirtualBox>
2017-11-16 16:49:18 +0100

    TestReprésentation et TestReprésentationDAO

---
+++ test/RepresentationDAOTest.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>RepresentationDAO : test</title>
+    </head>
+
+    <body>
+
+        <?php
+        use modele\dao\RepresentationDAO;
+        use modele\dao\Bdd;
+require_once __DIR__ . '/../includes/autoload.php';
+        $id = '3';
+        Bdd::connecter();
+        echo "<h2>Test RepresentationDAO</h2>";
+        // Test n°1
+        echo "<h3>Test getOneById</h3>";
+        try {
+            $objet = RepresentationDAO::getOneById($id);
+            var_dump($objet);
+        } catch (Exception $ex) {
+            echo "<h4>*** échec de la requête ***</h4>" . $ex->getMessage();
+        }
+        
+        echo "<h3>Test getAll</h3>";
+        try {
+            $lesObjets = RepresentationDAO::getAll();
+            var_dump($lesObjets);
+        } catch (Exception $ex) {
+            echo "<h4>*** échec de la requête ***</h4>" . $ex->getMessage();
+        }
+        
+        Bdd::deconnecter();
+        ?>
+
+    </body>
+    
+</html>
