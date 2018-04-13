71a9914aa4611b18b26a9675e2452ef1f6aa867c
trouault <trouault@btssio-VirtualBox>
2018-04-13 13:38:23 +0200

    Fin itération 6

--- vues/Login/vLogin.php
+++ vues/Login/vLogin.php
@@ -11,9 +11,8 @@
             <input type="text" name="login" class="sign-up-input" placeholder="Nom d'utilisateur"><br />
             <input type="password" name="pwd" class="sign-up-input" placeholder="Mot de passe"><br />
             <input type="submit" value="Connexion" class="sign-up-button">
+            <input type="reset" value="Annuler" class="sign-up-button">
         </form>
-        <form id="blocLogin2" action="cLogout.php" method="post">
-            <input type="submit" value="Annuler" class="sign-up-button">
-        </form>
+        
     </body>
 </html>
